c6d0e23894ddaa56354c5b45a65a241b547814f8
Cássio Sousa <cassiosantossousa@gmail.com>
2015-09-05 12:11:28 -0300

    Added meaningful comments

--- src/assorted/LifeAndEverything.java
+++ src/assorted/LifeAndEverything.java
@@ -3,7 +3,20 @@ package assorted;
 import java.util.*;
 import java.lang.*;
 
+/**
+ * Problem: receive numbers and print them until you receive the number 42. Don't print it.
+ *
+ * @author Cassio Sousa
+ * @version 1.0
+ */
 public class LifeAndEverything {
+
+    /**
+     * Main function.
+     *
+     * @param args Input arguments (none).
+     * @throws java.lang.Exception if any.
+     */
     public static void main(String[] args) throws java.lang.Exception {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();

fc72695c0f1d4cfc6e20daaf7de65867d2123887
cassioss <cassiosantossousa@gmail.com>
2015-10-26 16:46:32 -0200

    Added NextFromSequence and PointAndPattern

---
+++ src/assorted/NextFromSequence.java
@@ -0,0 +1,38 @@
+package assorted;
+
+import java.util.Scanner;
+
+/**
+ * @author Cassio dos Santos Sousa
+ * @version 1.0
+ */
+public class NextFromSequence {
+
+
+    /**
+     * Returns the next number from a sequence of three integers, along with the type of the progression (arithmetic
+     * or geometric).
+     */
+    private static String nextFromSequence(int a1, int a2, int a3) {
+        if (a1 + a3 == a2 + a2)
+            return "AP " + String.valueOf(a3 + (a2 - a1));
+        else
+            return "GP " + String.valueOf((a3 * a2) / a1);
+    }
+
+    /**
+     * Main function.
+     */
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int a1 = sc.nextInt();
+        int a2 = sc.nextInt();
+        int a3 = sc.nextInt();
+        while (!(a1 == 0 && a2 == 0 && a3 == 0)) {
+            System.out.println(nextFromSequence(a1, a2, a3));
+            a1 = sc.nextInt();
+            a2 = sc.nextInt();
+            a3 = sc.nextInt();
+        }
+    }
+}
---
+++ src/assorted/PointAndPattern.java
@@ -0,0 +1,44 @@
+package assorted;
+
+import java.util.Scanner;
+
+/**
+ * @author Cassio dos Santos Sousa
+ * @version 1.0
+ */
+public class PointAndPattern {
+
+    /**
+     * Checks, for a specific pattern for coordinates X and Y, if there is a number in that point, and if so, which
+     * is it.
+     */
+    private static String pointAndPattern(int x, int y) {
+        if (x < 0 || y < 0)
+            return "No Number";
+        if (x == y) {
+            if (x % 2 == 0)
+                return String.valueOf(2 * y);
+            else
+                return String.valueOf(2 * y - 1);
+        } else if (x - y == 2) {
+            if (x % 2 == 0)
+                return String.valueOf(2 * y + 2);
+            else
+                return String.valueOf(2 * y + 1);
+        } else return "No Number";
+    }
+
+    /**
+     * Main function.
+     */
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            System.out.println(pointAndPattern(x, y));
+        }
+    }
+
+}

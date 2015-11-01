e3268908cd9be030e34159ea3ac823f9a9365c6d
Cássio Sousa <cassiosantossousa@gmail.com>
2015-11-01 06:14:30 -0200

    Recheck Coins problem

---
+++ src/dynamic/Coins.java
@@ -0,0 +1,36 @@
+package dynamic;
+
+import java.util.*;
+
+/**
+ * @author Cassio dos Santos Sousa
+ * @version 1.0
+ */
+public class Coins {
+
+    public static int maxValue(int coin) {
+        int max = coin / 2 + coin / 3 + coin / 4;
+        if (max > coin)
+            return max;
+        else
+            return coin;
+    }
+
+    /**
+     * Main method.
+     *
+     * @param args input arguments (none).
+     * @throws Exception if any.
+     */
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        List<Integer> allInputs = new ArrayList<>();
+        while (sc.hasNext())
+            allInputs.add(Integer.valueOf(sc.next()));
+        System.out.println(allInputs);
+        for (Integer input : allInputs)
+            System.out.println(maxValue(input));
+
+    }
+
+}
